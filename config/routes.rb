0bbb0af706a94b8bfe4900be794d709509b2190b
degranjo <degranjo@mail.gvsu.edu>
2019-04-12 11:16:11 -0400

    update register redirection and add leagues page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :game_picks
+  resources :leagues
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'sessions#new'
   get 'signup', to: 'users#new', as: 'signup'
@@ -9,4 +10,5 @@ Rails.application.routes.draw do
   get 'logout', to: 'sessions#destroy', as: 'logout'
   get 'picks', to: 'game_picks#index', as: 'picks'
   get 'about', to: 'about#about', as: 'about'
+  get 'leagues', to: 'leagues#index', as: 'leagues'
 end
